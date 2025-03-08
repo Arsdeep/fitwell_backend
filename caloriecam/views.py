5b0efd4dd92013ac4900390d1a50c0c138e1c622
Arsdeep <arsgulu@gmail.com>
2025-03-08 17:23:03 +0530

    ✨ feat(api): enhance food analysis with macronutrient estimation
    - update prompt and response structure to include protein, carbs, and fat estimation
    - improve nutritional analysis by providing detailed macronutrient information

--- caloriecam/views.py
+++ caloriecam/views.py
@@ -18,19 +18,25 @@ class FoodCalorieAPIView(APIView):
             # Encode the image in Base64
             base64_image = base64.b64encode(image.read()).decode("utf-8")
             
-            prompt = "Your job is to Analyze the given image and identify all food items present. Provide a structured JSON response containing the number of detected food items, their names, and an estimated calorie count for each. Format the response as follows:\n\n"
+            prompt = "Your job is to Analyze the given image and identify all food items present. Provide a structured JSON response containing the number of detected food items, their names, and an estimated calorie+protien+carbs+fat count for each. Format the response as follows:\n\n"
             
             structure = """{
                 "foods": [
                     {
                     "name": "<food_name>",
                     "count": "<estimated_count>"
-                    "calories": "<estimated_calories>"
+                    "calories": "<estimated_calories>",
+                    "protien": "<estimated_protien>",
+                    "carbs": "<estimated_carbs>",
+                    "fat": "<estimated_fat>"
                     },
                     {
                     "name": "<food_name>",
                     "count": "<estimated_count>"
-                    "calories": "<estimated_calories>"
+                    "calories": "<estimated_calories>",
+                    "protien": "<estimated_protien>",
+                    "carbs": "<estimated_carbs>",
+                    "fat": "<estimated_fat>"
                     }
                 ]
                 }
